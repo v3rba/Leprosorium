1617e6c59cdb8a3d4fe935870098919199dce4ab
v3rba <verbush@gmail.com>
2018-10-13 14:57:10 +0300

    Add parameter validation

--- app.rb
+++ app.rb
@@ -48,8 +48,13 @@ end
 post "/new" do
 
   # получаем переменную из post-запроса
-  @content = params[:content]
+  content = params[:content]
 
-  erb "You typed #{@content}"
+  if content.length <= 0
+    @error = 'Type post text'
+    return erb :new
+  end
+
+  erb "You typed #{content}"
 end
 
