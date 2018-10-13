027861b17972ac32edc73483b9fbed1e8d0ace81
v3rba <verbush@gmail.com>
2018-10-13 15:20:57 +0300

    Redirect to main page

--- app.rb
+++ app.rb
@@ -64,6 +64,9 @@ post "/new" do
 
   @db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 
+  # перенаправление на главную страницу
+
+  redirect to '/'
   erb "You typed: #{content}"
 end
 
