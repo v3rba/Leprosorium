0253fe772ef8f46378b73b976f3c2c020c43c187
v3rba <verbush@gmail.com>
2018-10-13 15:43:08 +0300

    Add details view

--- app.rb
+++ app.rb
@@ -67,7 +67,6 @@ post "/new" do
   # перенаправление на главную страницу
 
   redirect to '/'
-  erb "You typed: #{content}"
 end
 
 # вывод информации о посте
@@ -75,7 +74,8 @@ end
 get '/details/:post_id' do
   post_id = params[:post_id]
 
-  erb "Displaying information for post with id #{post_id}"
+  results = @db.execute 'select * from Posts where id = ?', [post_id]
+  @row = results[0]
 
-
+  erb :details
 end
